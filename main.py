5e0b49bada1879978903667dc3394ffa2cfa13d4
luisp <luis.p.vicente@gmail.com>
2024-09-30 19:51:43 +0200

    Commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from photosignature import  sign_svg
+from photosignature import sign_svg
 
 
-sign_svg(Path(r'aaaaa.svg'), Path(r'DSC_3165.JPG'), title='Ola k ase', color='FFFFFF')
+sign_svg(Path(r'aaaaa.svg'), Path(r'DSC_3165.JPG'), title='Berrea 2024', signature='Luis Pedro Vicente Matilla',
+         color='000000')
--- photosignature/__init__.py
+++ photosignature/__init__.py
@@ -1,15 +1,21 @@
 from pathlib import Path
 import base64
 from jinja2 import Environment, PackageLoader
+from PIL import Image
 
 
-def sign_svg(out_svg: Path, input_image: Path, title: str = '', signature: str = '', color='808080') -> None:
+def sign_svg(out_svg: Path, input_image: Path, title: str = '', signature: str = '',
+             color='808080', anchor='end') -> None:
     environment = Environment(loader=PackageLoader('photosignature'))
     template = environment.get_template(r'svg_template.svg')
+
+    im = Image.open(input_image)
+    w, h = im.size
 
     with open(input_image, 'rb') as png_file:
         encoded_string = base64.b64encode(png_file.read()).decode('utf-8')
 
     with open(out_svg, 'w', encoding='utf-8') as svg_file:
-        content = template.render(image_data=encoded_string, color=color, title=title, signature=signature)
+        content = template.render(image_data=encoded_string, height=h, width=w, anchor=anchor,
+                                  color=color, title=title, signature=signature)
         svg_file.write(content)
